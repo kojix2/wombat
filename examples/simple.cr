e616e365d385c8da5c58beb5c3a5507e95fad13e
kojix2 <2xijok@gmail.com>
2024-08-12 07:14:21 +0900

    Improved example

--- examples/simple.cr
+++ examples/simple.cr
@@ -1,7 +1,10 @@
 require "../src/wombat"
 
-Wombat.pretty_print(Path[__FILE__])
+# Output the file content with syntax highlighting by calling the rust function
+Wombat.pretty_print(path: Path[__FILE__])
 
+# Output the highlighted string of the input by calling the rust function
+Wombat.pretty_print(input: %{fn main() { println!("Hello, world!"); }}, language: "Rust")
+
+# Get the highlighted string of the input
 puts Wombat.pretty_string(%{puts "Hello, World!"}, language: "Crystal", theme: "TwoDark")
-
-puts Wombat.bat_c_version
