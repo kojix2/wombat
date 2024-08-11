d46463b424dce7c4e21e2057a53e060bfc63ff25
kojix2 <2xijok@gmail.com>
2024-08-12 07:03:07 +0900

    Change method order

--- src/wombat.cr
+++ src/wombat.cr
@@ -9,51 +9,6 @@ module Wombat
     String.new(Wombat::Bat.bat_c_version)
   end
 
-  def self.pretty_print(
-    path : (Path | String),
-    language : String? = nil,
-    theme : String? = nil,
-    tab_width : Int = 4,
-    colored_output : Bool = true,
-    true_color : Bool = true,
-    header : Bool = true,
-    line_numbers : Bool = true,
-    grid : Bool = true,
-    rule : Bool = true,
-    show_nonprintable : Bool = false,
-    snip : Bool = true,
-    wrapping_mode : Int = 1,
-    use_italics : Bool = true,
-    paging_mode : Int = 1,
-    highlight_line : Int = 1
-  ) : Nil
-    result = Wombat::Bat.bat_pretty_print(
-      path.to_s,
-      path.to_s.size,
-      Wombat::Bat::BatInputType::BatFile,
-      language,
-      theme,
-      Wombat::Bat::BatPrintOptions.new(
-        tab_width: tab_width,
-        colored_output: colored_output,
-        true_color: true_color,
-        header: header,
-        line_numbers: line_numbers,
-        grid: grid,
-        rule: rule,
-        show_nonprintable: show_nonprintable,
-        snip: snip,
-        wrapping_mode: wrapping_mode,
-        use_italics: use_italics,
-        paging_mode: paging_mode,
-        highlight_line: highlight_line
-      )
-    )
-    if result != 0
-      raise Error.new("Failed to pretty print file : #{path}")
-    end
-  end
-
   # Pretty print the input string
   def self.pretty_print(
     input : String,
@@ -97,6 +52,52 @@ module Wombat
     )
     if result != 0
       raise Error.new("Failed to pretty print input")
+    end
+  end
+
+  # Pretty print the input file
+  def self.pretty_print(
+    path : (Path | String),
+    language : String? = nil,
+    theme : String? = nil,
+    tab_width : Int = 4,
+    colored_output : Bool = true,
+    true_color : Bool = true,
+    header : Bool = true,
+    line_numbers : Bool = true,
+    grid : Bool = true,
+    rule : Bool = true,
+    show_nonprintable : Bool = false,
+    snip : Bool = true,
+    wrapping_mode : Int = 1,
+    use_italics : Bool = true,
+    paging_mode : Int = 1,
+    highlight_line : Int = 1
+  ) : Nil
+    result = Wombat::Bat.bat_pretty_print(
+      path.to_s,
+      path.to_s.size,
+      Wombat::Bat::BatInputType::BatFile,
+      language,
+      theme,
+      Wombat::Bat::BatPrintOptions.new(
+        tab_width: tab_width,
+        colored_output: colored_output,
+        true_color: true_color,
+        header: header,
+        line_numbers: line_numbers,
+        grid: grid,
+        rule: rule,
+        show_nonprintable: show_nonprintable,
+        snip: snip,
+        wrapping_mode: wrapping_mode,
+        use_italics: use_italics,
+        paging_mode: paging_mode,
+        highlight_line: highlight_line
+      )
+    )
+    if result != 0
+      raise Error.new("Failed to pretty print file : #{path}")
     end
   end
 
