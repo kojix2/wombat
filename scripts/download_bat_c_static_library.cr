6d93bbfa261aca4f2ba61b32c79c05ff593c5788
kojix2 <2xijok@gmail.com>
2025-11-10 19:32:43 +0900

    crystal tool format

--- scripts/download_bat_c_static_library.cr
+++ scripts/download_bat_c_static_library.cr
@@ -1,10 +1,10 @@
 {% if flag?(:darwin) %}
-   system(File.expand_path("download_bat_c_static_library.sh", __DIR__))
+  system(File.expand_path("download_bat_c_static_library.sh", __DIR__))
 {% elsif flag?(:linux) %}
-   system(File.expand_path("download_bat_c_static_library.sh", __DIR__))
+  system(File.expand_path("download_bat_c_static_library.sh", __DIR__))
 {% elsif flag?(:windows) %}
-   system(File.expand_path("download_bat_c_static_library.bat", __DIR__))
+  system(File.expand_path("download_bat_c_static_library.bat", __DIR__))
 {% else %}
-   puts "Unsupported platform"
-   exit 1
+  puts "Unsupported platform"
+  exit 1
 {% end %}
